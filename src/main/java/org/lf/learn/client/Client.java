302063fa2a13b1802c2677112855efb82ede4444
liufu1 <liufu1@yy.com>
2022-10-26 23:27:54 +0800

    add

---
+++ src/main/java/org/lf/learn/client/Client.java
@@ -0,0 +1,25 @@
+package org.lf.learn.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class Client {
+    public static void main(String[] args) throws Exception{
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+
+        try{
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class)
+                    .handler(new MyClientInitializer());
+
+            //ChannelFuture channelFuture = bootstrap.connect("47.113.192.207",8899).sync();
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1",8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+    }
+}
