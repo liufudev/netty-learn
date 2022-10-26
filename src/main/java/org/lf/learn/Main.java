8733d11ca5286440b482ae6a3803fcdc6d4ac5c7
liufu1 <liufu1@yy.com>
2022-10-26 23:51:16 +0800

    add

--- src/main/java/org/lf/learn/Main.java
+++ src/main/java/org/lf/learn/Main.java
@@ -1,7 +1,13 @@
 package org.lf.learn;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Logger logger = LoggerFactory.getLogger("Test");
+        logger.info("asdasd");
     }
 }
