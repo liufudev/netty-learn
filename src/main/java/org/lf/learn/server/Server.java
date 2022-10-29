b7a5152954444bf987ee8a90396d809b8c33bdcf
liufu1 <liufu1@yy.com>
2022-10-29 23:01:54 +0800

    add

--- src/main/java/org/lf/learn/server/Server.java
+++ src/main/java/org/lf/learn/server/Server.java
@@ -13,19 +13,19 @@ public class Server {
     public static void main(String[] args) throws InterruptedException {
         log.info("main param is [args] ");
         EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup wokerGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try{
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             //在服务器端的handler()方法表示对bossGroup起作用，而childHandler表示对wokerGroup起作用
-            serverBootstrap.group(bossGroup,wokerGroup).channel(NioServerSocketChannel.class)
+            serverBootstrap.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class)
                     .childHandler(new MyServerInitializer());
 
             ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
             channelFuture.channel().closeFuture().sync();
         }finally {
             bossGroup.shutdownGracefully();
-            wokerGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
         }
     }
 }
