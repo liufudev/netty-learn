c7888b0452f5319b0e4dd9dccd17b5f53bc22f5b
liufu1 <liufu1@yy.com>
2022-11-08 22:32:20 +0800

    add

--- src/main/java/org/lf/learn/server/EchoServer.java
+++ src/main/java/org/lf/learn/server/EchoServer.java
@@ -5,6 +5,8 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.CharsetUtil;
 
 public class EchoServer {
 
@@ -16,7 +18,8 @@ public class EchoServer {
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     protected void initChannel(final SocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(new EchoServerHandler());
+                        ch.pipeline().addLast(new StringDecoder(CharsetUtil.UTF_8))
+                                .addLast(new EchoServerHandler());
                     }
                 });
         bootstrap.bind().sync();
