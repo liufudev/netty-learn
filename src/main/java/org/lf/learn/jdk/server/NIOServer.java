86003156b10356c4c5ecc181305490016513dfb2
liufu1 <liufu1@yy.com>
2022-11-14 09:27:52 +0800

    add

--- src/main/java/org/lf/learn/jdk/server/NIOServer.java
+++ src/main/java/org/lf/learn/jdk/server/NIOServer.java
@@ -1,91 +1,60 @@
 package org.lf.learn.jdk.server;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.SocketException;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.Charset;
-import java.util.Iterator;
-import java.util.Set;
 
 public class NIOServer {
-    public static void main(String[] args) throws IOException {
-        Selector serverSelector = Selector.open();
-        Selector clientSelector = Selector.open();
-
-        new Thread(() -> {
-            try {
-                // 对应IO编程中服务端启动
-                ServerSocketChannel listenerChannel = ServerSocketChannel.open();
-                listenerChannel.socket().bind(new InetSocketAddress(8000));
-                listenerChannel.configureBlocking(false);
-                listenerChannel.register(serverSelector, SelectionKey.OP_ACCEPT);
-
-                while (true) {
-                    // 监测是否有新的连接，这里的1指的是阻塞的时间为 1ms
-                    if (serverSelector.select(1) > 0) {
-                        Set<SelectionKey> set = serverSelector.selectedKeys();
-                        Iterator<SelectionKey> keyIterator = set.iterator();
-
-                        while (keyIterator.hasNext()) {
-                            SelectionKey key = keyIterator.next();
-
-                            if (key.isAcceptable()) {
-                                try {
-                                    // (1) 每来一个新连接，不需要创建一个线程，而是直接注册到clientSelector
-                                    SocketChannel clientChannel = ((ServerSocketChannel) key.channel()).accept();
-                                    clientChannel.configureBlocking(false);
-                                    clientChannel.register(clientSelector, SelectionKey.OP_READ);
-                                } finally {
-                                    keyIterator.remove();
-                                }
-                            }
-
-                        }
-                    }
+    public static void main(String[] args) {
+        byte[] data = "hello".getBytes();
+        SocketChannel channel = null;
+        try {
+            // 1. 创建 SocketChannel 实例，并配置为非阻塞模式，只有在非阻塞模式下，任何在 SocketChannel 实例上的 I/O
+            // 操作才是非阻塞的。这样我们的客户端就是一个非阻塞式客户端，也就可以提升客户端性能。
+            channel = SocketChannel.open();
+            channel.configureBlocking(false);
+            // 2. 用 connect() 方法连接服务器，同时用 while 循环不断检测并完全连接。 其实我们可以不用这样盲等，这里只是为了演示连接的过程。
+            // 当你在需要马上进行 I/O 操作前，必须要用 finishConnect() 完成连接过程。
+            if (!channel.connect(new InetSocketAddress(InetAddress.getLocalHost(), 8899))) {
+                while (!channel.finishConnect()) {
+                    System.out.print("正在尝试连接服务器.....");
                 }
-            } catch (IOException ignored) {
             }
 
-        }).start();
+            System.out.println("服务器连接成功!");
 
-
-        new Thread(() -> {
-            try {
-                while (true) {
-                    // (2) 批量轮询是否有哪些连接有数据可读，这里的1指的是阻塞的时间为 1ms
-                    if (clientSelector.select(1) > 0) {
-                        Set<SelectionKey> set = clientSelector.selectedKeys();
-                        Iterator<SelectionKey> keyIterator = set.iterator();
-
-                        while (keyIterator.hasNext()) {
-                            SelectionKey key = keyIterator.next();
-
-                            if (key.isReadable()) {
-                                try {
-                                    SocketChannel clientChannel = (SocketChannel) key.channel();
-                                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-                                    // (3) 面向 Buffer
-                                    clientChannel.read(byteBuffer);
-                                    byteBuffer.flip();
-                                    System.out.println(Charset.defaultCharset().newDecoder().decode(byteBuffer)
-                                            .toString());
-                                } finally {
-                                    keyIterator.remove();
-                                    key.interestOps(SelectionKey.OP_READ);
-                                }
-                            }
-
-                        }
-                    }
+            ByteBuffer writeBuffer = ByteBuffer.wrap(data);
+            ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+            int totalBytesReceived = 0;
+            int bytesReceived;
+            // 3. 用 ByteBuffer 读写字节，这里我们为何和一个 while 循环不断地读写呢？ 还记得前面讲 SelectableChannel
+            // 非阻塞时的特性吗？ 如果一个 SelectableChannel 为非阻塞模式，它的 I/O 操作读写的字节数可能比实际的要少，甚至没有。
+            // 所以我们这里用循环不断的读写，保证读写完成。
+            while (totalBytesReceived < data.length) {
+                if (writeBuffer.hasRemaining()) {
+                    channel.write(writeBuffer);
                 }
-            } catch (IOException ignored) {
+                if ((bytesReceived = channel.read(readBuffer)) == -1) {
+                    throw new SocketException("Connection closed prematurely");
+                }
+                totalBytesReceived += bytesReceived;
+                System.out.println("等待服务器回应.....");
             }
-        }).start();
-
-
+            System.out.println("Server said: " + new String(readBuffer.array()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 4 .close socket channel
+            try {
+                if (channel != null) {
+                    channel.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
